1f061b3f7cba976347854d7fbe7911c769a09f9b
Taite Jernigan <taite5100@gmail.com>
2023-02-22 20:49:25 +0000

    Completed Lab Set

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,5 @@
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * Sets are unordered collections of elements. Sets do not allow duplicates.
@@ -20,7 +20,7 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        return new HashSet<Integer>();
     }
 
     /**
@@ -29,7 +29,7 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
@@ -39,7 +39,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        set.add(item);
     }
 
     /**
@@ -49,6 +49,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
